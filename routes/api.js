f7b5fc8244a5260c99ee59c6dc83ded7700b7ede
Carolina Cruz <caro21_cruz@hotmail.com>
2022-12-01 23:49:13 -0600

    se cambio ruta de get y post

--- routes/api.js
+++ routes/api.js
@@ -1,39 +1,30 @@
 const router = require('express').Router();
 const fs = require('fs');
-// Helper method for generating unique ids
-const { v4: uuidv4 } = require('uuid');
-const util = require("util");
 
-//Ruta get para notas
+const { v4: uuidv4 } = require('uuid');
+const data = require('../db/db.json');
+
+
+
+//Ruta get para mandar notas que tenga
 router.get('/api/notes', (req,res)=>{
-    const read = util.promisify(fs.readFile);
-    read("./db/db.json").then((data) => res.json(JSON.parse(data)))
-    
-});
+    res.send(data);
+})
 //Ruta post para agregar nota
-router.post('/api/notes', (req,res)=>{
-    if(req.body){
-        const newN={
-            title: req.body.title,
-            text: req.body.text,
-            id:uuidv4(),
-        }
-         fs.readFile("./db/db.json",'utf-8',(error,data)=>{
-            if(error){
-                console.error(error);
-            }else{
-                const array = JSON.parse(data);
-                array.push(newN);
-                fs.writeFile("./db/db.json",JSON.stringify(array), (error) => {
-                    error ? console.error(err) : console.info(`\nSe agrego la nota`)
-                });
-            };
-    
-        });
-    }else{
-        res.error('No hay nota nueva')
-    }
-   
+router.post('/api/notes', (req, res) => {
+    const newN={
+         title: req.body.title,
+          text: req.body.text,
+          id:uuidv4(),
+      }
+    // crear array si no hay notas o usar la que ya esta
+    const array=data || [];
+    array.push(newN);
+    fs.writeFile('./db/db.json', JSON.stringify(array), (err) => {
+        if (err) throw err;
+    });
+    console.info('La nota se agrego a JASON db');
+    res.json();
 });
 //Borrar nota con parametro id
 router.delete('/api/notes/:id', (req, res) => {
